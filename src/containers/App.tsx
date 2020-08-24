1e8c81cb11183ae0276ecec0d0d5f255140f3d67
Yunfei Jing <jingy277@gmail.com>
2020-08-24 22:55:28 +1000

    restructure project and add signin page

---
+++ src/containers/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import SignIn from '../containers/SignIn/SignIn';
+
+function App() {
+  return (
+    <div className="App">
+      <SignIn />
+    </div>
+  );
+}
+
+export default App;
