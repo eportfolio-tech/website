fc102393d80bbb2cd1eaf4f9ba0b0cd28ed0778a
Yunfei Jing <jingy277@gmail.com>
2020-09-02 18:10:20 +1000

    modify signup page and change api url

--- src/components/AuthDialogs/signUpForm.tsx
+++ src/components/AuthDialogs/signUpForm.tsx
@@ -1,210 +1,311 @@
-import React from 'react';
-import {makeStyles, createStyles, Theme} from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Stepper from '@material-ui/core/Stepper';
-import Step from '@material-ui/core/Step';
-import StepLabel from '@material-ui/core/StepLabel';
-import Button from '@material-ui/core/Button';
-import Link from '@material-ui/core/Link';
-import Typography from '@material-ui/core/Typography';
-import AccountForm from '../Forms/AccountForm/AccountForm';
-import OptionalForm from '../Forms/OptionalForm/OptionalForm';
-import ReviewForm from '../Forms/ReviewForm/ReviewForm';
-import {userService} from '../../services/userService';
+import React from "react";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import Link from "@material-ui/core/Link";
+import Typography from "@material-ui/core/Typography";
+import { userService } from "../../services/userService";
+import { TextField, Grid, MenuItem } from "@material-ui/core";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/material.css";
+import { useSnackbar } from "notistack";
 
 function Copyright() {
-    return (
-        <Typography variant="body2" color="textSecondary" align="center">
-            {'Copyright © '}
-            <Link color="inherit" href="https://dev.eportfolio.tech/">
-                COMP30022
-            </Link>{' '}
-            {new Date().getFullYear()}
-            {'.'}
-        </Typography>
-    );
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://dev.eportfolio.tech/">
+        COMP30022
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
 }
 
+const titles = [
+  {
+    value: "Mr.",
+    label: "Mr.",
+  },
+  {
+    value: "Mrs.",
+    label: "Mrs.",
+  },
+  {
+    value: "Ms.",
+    label: "Ms.",
+  },
+  {
+    value: "Miss",
+    label: "Miss",
+  },
+  {
+    value: "Dr.",
+    label: "Dr.",
+  },
+];
+
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        appBar: {
-            position: 'relative',
-        },
-        layout: {
-            width: 'auto',
-            marginLeft: theme.spacing(2),
-            marginRight: theme.spacing(2),
-            [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
-                width: 600,
-                marginLeft: 'auto',
-                marginRight: 'auto',
-            },
-        },
-        paper: {
-            marginTop: theme.spacing(3),
-            marginBottom: theme.spacing(3),
-            padding: theme.spacing(2),
-            [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
-                marginBottom: theme.spacing(6),
-                padding: theme.spacing(3),
-            },
-        },
-        stepper: {
-            padding: theme.spacing(3, 0, 5),
-        },
-        buttons: {
-            display: 'flex',
-            justifyContent: 'flex-end',
-        },
-        button: {
-            marginTop: theme.spacing(3),
-            marginLeft: theme.spacing(1),
-        },
-    })
+  createStyles({
+    appBar: {
+      position: "relative",
+    },
+    layout: {
+      width: "auto",
+      marginLeft: theme.spacing(2),
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+        width: 600,
+        marginLeft: "auto",
+        marginRight: "auto",
+      },
+    },
+    paper: {
+      marginTop: theme.spacing(3),
+      marginBottom: theme.spacing(3),
+      padding: theme.spacing(2),
+      [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+        marginBottom: theme.spacing(6),
+        padding: theme.spacing(3),
+      },
+    },
+    stepper: {
+      padding: theme.spacing(3, 0, 5),
+    },
+    buttons: {
+      display: "flex",
+      justifyContent: "flex-end",
+    },
+    button: {
+      marginTop: theme.spacing(3),
+      marginLeft: theme.spacing(1),
+    },
+  })
 );
 
-const steps = ['Enter details', 'Optional details', 'Verification'];
-
-// interface ISignUpForm {
-// 	setOpen: any;
-// }
-
 export default () => {
-    const classes = useStyles();
-    const [activeStep, setActiveStep] = React.useState<number>(0);
+  const classes = useStyles();
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  const [userInfo, setUserInfo] = React.useState<any | null>({
+    password: "",
+    title: "",
+  });
 
-    const [userInfo, setUserInfo] = React.useState<any | null>({
-        password: '',
-        title: '',
-        role: '',
-    });
+  const handleInput = (key: any, value: any) => {
+    const newUserInfo = {
+      ...userInfo,
+      [key]: value,
+    };
+    setUserInfo(newUserInfo);
+  };
 
-    function getStepContent(step: number) {
-        switch (step) {
-            case 0:
-                return (
-                    <AccountForm
-                        info={userInfo}
-                        handle={handleInput}
-                        check={checkPassword}
-                    />
-                );
-            case 1:
-                return (
-                    <OptionalForm
-                        info={userInfo}
-                        handle={handleInput}
-                        handlePhone={handlePhone}
-                    />
-                );
-            case 2:
-                return <ReviewForm />;
-            default:
-                throw new Error('Unknown step');
-        }
+  const checkPassword = () => {
+    if (userInfo.password.length === 0) {
+      // to not dispplay error when empty
+      return true;
     }
-
-    const handleNext = () => {
-        if (activeStep === 2) {
-            onSignUpHandler();
-        }
-        setActiveStep(activeStep + 1);
-    };
-
-    const handleBack = () => {
-        setActiveStep(activeStep - 1);
-    };
-
-    const handleInput = (propertyName: any) => (event: {
-        target: {value: any};
-    }) => {
-        const newUserInfo = {
-            ...userInfo,
-            [propertyName]: event.target.value,
-        };
-        setUserInfo(newUserInfo);
-        // console.log(userInfo);
-    };
-
-    const handlePhone = (phone: string) => {
-        const newUserInfo = {
-            ...userInfo,
-            phone: phone,
-        };
-        setUserInfo(newUserInfo);
-    };
-
-    const checkPassword = () => {
-        // console.log(userInfo.password);
-        if (userInfo.password.length < 8 && userInfo.password.length > 0) {
-            return false;
-        }
-        return true;
-    };
-
-    const onSignUpHandler = async () => {
-        try {
-            await userService.signup(userInfo);
-        } catch (error) {
-            console.log('Sign up failed');
-        }
-    };
-
+    if (userInfo.password.length < 8) {
+      return false;
+    }
+    if (/\s/.test(userInfo.password)) {
+      return false;
+    }
     return (
-        <main className={classes.layout}>
-            <Paper className={classes.paper}>
-                <Stepper activeStep={activeStep} className={classes.stepper}>
-                    {steps.map((label) => (
-                        <Step key={label}>
-                            <StepLabel>{label}</StepLabel>
-                        </Step>
-                    ))}
-                </Stepper>
-
-                {activeStep === steps.length ? (
-                    <div>
-                        <Typography variant="h5" gutterBottom>
-                            Registration Succeed.
-                        </Typography>
-                        <Typography variant="subtitle1">
-                            You can manage your personal details under your
-                            dashboard.
-                        </Typography>
-                    </div>
-                ) : (
-                    <div>
-                        {getStepContent(activeStep)}
-                        <div className={classes.buttons}>
-                            {activeStep !== 0 && (
-                                <Button
-                                    onClick={handleBack}
-                                    className={classes.button}>
-                                    Back
-                                </Button>
-                            )}
-                            <Button
-                                variant="contained"
-                                color="primary"
-                                onClick={handleNext}
-                                className={classes.button}
-                                disabled={
-                                    !userInfo.firstName ||
-                                    !userInfo.lastName ||
-                                    !userInfo.email ||
-                                    !userInfo.password ||
-                                    !userInfo.repassword ||
-                                    userInfo.password !== userInfo.repassword ||
-                                    !checkPassword
-                                }>
-                                {activeStep === steps.length - 1
-                                    ? 'Done'
-                                    : 'Next'}
-                            </Button>
-                        </div>
-                    </div>
-                )}
-            </Paper>
-            <Copyright />
-        </main>
+      /[a-z]/.test(userInfo.password) &&
+      /[A-Z]/.test(userInfo.password) &&
+      /\d/.test(userInfo.password)
     );
+  };
+
+  const onSignUpHandler = async () => {
+    try {
+      await userService.signup(userInfo);
+      enqueueSnackbar("sign up succeed", {
+        variant: "success",
+      });
+    } catch (error) {
+      enqueueSnackbar("sign up failed", {
+        variant: "error",
+      });
+    }
+  };
+
+  return (
+    <main className={classes.layout}>
+      <Paper className={classes.paper}>
+        <div>
+          <Typography variant="h6" gutterBottom>
+            Enter Details
+          </Typography>
+          <Grid container spacing={4}>
+            <Grid item xs={12} md={6}>
+              <TextField
+                id="outlined-select-currency"
+                value={userInfo.title}
+                select
+                label="Select Title"
+                fullWidth
+                onChange={(event) => handleInput("title", event.target.value)}
+                variant="outlined"
+              >
+                {titles.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <TextField
+                id="preferedname"
+                label="Prefered Name"
+                fullWidth
+                autoComplete="cc-name"
+                defaultValue={userInfo.preferedName}
+                onChange={(event) =>
+                  handleInput("preferedname", event.target.value)
+                }
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                id="firstName"
+                name="firstName"
+                label="First name"
+                fullWidth
+                autoComplete="given-name"
+                defaultValue={userInfo.firstName}
+                onChange={(event) =>
+                  handleInput("firstName", event.target.value)
+                }
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                id="lastName"
+                name="lastName"
+                label="Last name"
+                fullWidth
+                autoComplete="family-name"
+                defaultValue={userInfo.lastName}
+                onChange={(event) =>
+                  handleInput("lastName", event.target.value)
+                }
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                id="email"
+                name="email"
+                label="Email Address"
+                fullWidth
+                autoComplete="Email Address"
+                defaultValue={userInfo.email}
+                onChange={(event) => handleInput("email", event.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                id="password"
+                name="password"
+                label="Password (with minimum eight characters)"
+                fullWidth
+                autoComplete="Password"
+                onChange={(event) =>
+                  handleInput("password", event.target.value)
+                }
+                error={!checkPassword()}
+                defaultValue={userInfo.password}
+                type="password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                id="repassword"
+                name="repassword"
+                label="Please Re-Enter your Password"
+                fullWidth
+                autoComplete="Re-Enter Password"
+                type="password"
+                onChange={(event) =>
+                  handleInput("repassword", event.target.value)
+                }
+                defaultValue={userInfo.repassword}
+                error={
+                  userInfo.password !== userInfo.repassword &&
+                  userInfo.password.length !== 0
+                }
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <PhoneInput
+                country={"au"}
+                enableAreaCodes={false}
+                onChange={(phone) => handleInput("phone", phone)}
+                value={userInfo.phone}
+                inputStyle={{
+                  width: "100%",
+                }}
+              />
+            </Grid>
+            {/* <Grid item xs={12} md={6}>
+              <TextField
+                id="zip"
+                label="ZIP"
+                fullWidth
+                autoComplete="zip"
+                defaultValue={this.props.info.zip}
+                onChange={this.props.handle("zip")}
+              />
+            </Grid> */}
+
+            {/* <Grid item xs={12}>
+              <TextField
+                id="role"
+                select
+                fullWidth
+                label="Which role can best describe you?"
+                onChange={this.props.handle("role")}
+                value={this.props.info.role}
+                variant="outlined"
+              >
+                {this.roles.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Grid> */}
+          </Grid>
+          <div className={classes.buttons}>
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              onClick={onSignUpHandler}
+              disabled={
+                !userInfo.firstName ||
+                !userInfo.lastName ||
+                !userInfo.email ||
+                !userInfo.password ||
+                !userInfo.repassword ||
+                userInfo.password !== userInfo.repassword ||
+                !checkPassword
+              }
+            >
+              Register
+            </Button>
+          </div>
+        </div>
+      </Paper>
+      <Copyright />
+    </main>
+  );
 };
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,45 +1,37 @@
-import axios from '../helper/axios';
-import {sha256} from 'js-sha256';
+import axios from "../helper/axios";
+import { sha256 } from "js-sha256";
 
 export const userService = {
-    // login,
-    // logout,
-    signup,
+  login,
+  logout,
+  signup,
 };
 
-// async function login(email, password) {
-//     let response = await axios.post(
-//         '/api/auth/login/',
-//         null,
-//         {
-//             params: {
-//                 email: email,
-//                 password: sha256.hmac('upathway', password),
-//             }
-//         }
-//     );
-//     const user = response.data.data;
-//     localStorage.setItem('user', JSON.stringify(user));
-//     return user;
-// }
+async function login(email, password) {
+  let response = await axios.post("/api/auth/login/", null, {
+    params: {
+      email: email,
+      password: sha256.hmac("upathway", password),
+    },
+  });
+  const user = response.data.data;
+  localStorage.setItem("user", JSON.stringify(user));
+  return user;
+}
 
-// async function logout(){
-//     await axios.post(
-//         '/api/auth/logout/',
-//         null,
-//         null
-//     );
-//     // remove user from local storage to log user out
-//     localStorage.removeItem('user');
-// }
+async function logout() {
+  await axios.post("/api/auth/logout/", null, null);
+  // remove user from local storage to log user out
+  localStorage.removeItem("user");
+}
 
 async function signup(userInfo) {
-    const response = await axios.post('/users/', {
-        firstName: userInfo.firstName,
-        lastName: userInfo.lastName,
-        email: userInfo.email,
-        password: sha256.hmac('e-portfolio', userInfo.password),
-    });
-    // console.log(response);
-    return response.data;
+  const response = await axios.post("/authentication/signup", {
+    firstName: userInfo.firstName,
+    lastName: userInfo.lastName,
+    email: userInfo.email,
+    password: sha256.hmac("e-portfolio", userInfo.password),
+  });
+  // console.log(response);
+  return response.data;
 }
