df474161703ed5b65abe2f347421263db991b179
Zijian Zeng <zijian@student.unimelb.edu.au>
2020-09-05 02:08:12 +1000

    fixing test failed

--- src/components/AuthDialogs/textField.tsx
+++ src/components/AuthDialogs/textField.tsx
@@ -1,35 +1,30 @@
-import React from 'react';
-import {TextField} from '@material-ui/core';
-import {useSelector} from 'react-redux';
-import {IRootState} from '../../index';
+import React from "react";
+import { TextField } from "@material-ui/core";
 
 interface ITextField {
-    label: string;
-    required?: boolean;
-    setState: any;
-    type?: string;
+	label: string;
+	required?: boolean;
+	setState: any;
+	type?: string;
 }
 
 /***
  * Login field.
  */
-export default ({label, required, setState, type}: ITextField) => {
-    const alert = useSelector<IRootState, any>((state) => state.alert);
-
-    return (
-        <TextField
-            variant="filled"
-            margin="normal"
-            required={required}
-            fullWidth
-            id={label}
-            label={label}
-            autoComplete={label}
-            type={type}
-            onChange={(e) => {
-                setState(e.target.value);
-            }}
-            error={alert.type === 'error'}
-        />
-    );
+export default ({ label, required, setState, type }: ITextField) => {
+	return (
+		<TextField
+			variant="filled"
+			margin="normal"
+			required={required}
+			fullWidth
+			id={label}
+			label={label}
+			autoComplete={label}
+			type={type}
+			onChange={(e) => {
+				setState(e.target.value);
+			}}
+		/>
+	);
 };
