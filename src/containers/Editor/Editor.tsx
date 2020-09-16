eaccc140598c276ebd8aaf279bdda516db42bf66
Yunfei Jing <yunfei.jing@outlook.com>
2020-09-16 10:39:02 +1000

    fix editor (version issue)

--- src/containers/Editor/Editor.tsx
+++ src/containers/Editor/Editor.tsx
@@ -1,5 +1,5 @@
 import React, {useRef} from 'react';
-import {makeStyles, createStyles, Theme} from '@material-ui/core/styles';
+import {makeStyles, createStyles} from '@material-ui/core/styles';
 // import {render} from 'react-dom';
 
 import EmailEditor from 'react-email-editor';
@@ -10,7 +10,7 @@ import {Button} from '@material-ui/core';
 import {useDispatch} from 'react-redux';
 import {alertActions} from '../../store/actions/alertActions';
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles((theme) =>
     createStyles({
         button: {
             marginTop: theme.spacing(3),
@@ -18,13 +18,15 @@ const useStyles = makeStyles((theme: Theme) =>
     })
 );
 
-const Editor = (props: any) => {
+export default (props: any) => {
     const classes = useStyles();
     const emailEditorRef = useRef(null);
     const dispatch = useDispatch();
 
     const exportHtml = () => {
-        (emailEditorRef.current as any).editor.exportHtml(
+        // Notice the difference btween this version (1.0.0 and the latest 1.1.1)
+        // Use emailEditorRef.current instead of emailEditorRef.current.editor
+        (emailEditorRef.current as any).exportHtml(
             (data: {design: any; html: any}) => {
                 const {design, html} = data;
                 // console.log('exportHtml', html);
@@ -37,17 +39,20 @@ const Editor = (props: any) => {
     };
 
     const onLoad = () => {
-        const design = JSON.parse(localStorage.getItem('design') || '');
-        (emailEditorRef.current as any).editor.loadDesign(design);
+        // you can load your template here;
+        // const templateJson = {};
+        // emailEditorRef.current.editor.loadDesign(templateJson);
+        const saved = JSON.parse(localStorage.getItem('design') || '');
+        (emailEditorRef.current as any).loadDesign(saved);
     };
 
     return (
         <div>
-            <div>
+            <div className="App">
                 <EmailEditor
                     ref={emailEditorRef}
-                    onLoad={onLoad}
                     minHeight={700}
+                    onLoad={onLoad}
                 />
             </div>
 
@@ -63,5 +68,3 @@ const Editor = (props: any) => {
         </div>
     );
 };
-
-export default Editor;
