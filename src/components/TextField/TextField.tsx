e165c666445bc0b9835974c5b2059d38e6db2db1
Xiancheng Yang <xiancheng@student.unimelb.edu.au>
2020-09-21 21:41:52 +1000

    fix TextField

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,125 +1,31 @@
+
 import React from 'react';
-import clsx from 'clsx';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import MuiTextField from '@material-ui/core/TextField';
-import { capitalize } from '@material-ui/core/utils';
+import {TextField} from '@material-ui/core';
 
-const styles = (theme: { spacing: (arg0: number) => any; palette: { common: { white: any; }; divider: any; secondary: { main: any; }; }; }) => ({
-    root: {
-        padding: 0,
-        'label + &': {
-            marginTop: theme.spacing(3),
-        },
-    },
-    input: {
-        minWidth: theme.spacing(6),
-        backgroundColor: theme.palette.common.white,
-        '&$disabled': {
-            backgroundColor: theme.palette.divider,
-        },
-    },
-    inputBorder: {
-        border: '1px solid #e9ddd0',
-        '&:focus': {
-            borderColor: theme.palette.secondary.main,
-        },
-    },
-    disabled: {},
-    inputSizeSmall: {
-        fontSize: 14,
-        padding: theme.spacing(1),
-        width: `calc(100% - ${theme.spacing(2)}px)`,
-    },
-    inputSizeMedium: {
-        fontSize: 16,
-        padding: theme.spacing(2),
-        width: `calc(100% - ${theme.spacing(4)}px)`,
-    },
-    inputSizeLarge: {
-        fontSize: 18,
-        padding: 22,
-        width: `calc(100% - ${22 * 2}px)`,
-    },
-    inputSizeXlarge: {
-        fontSize: 20,
-        padding: 25,
-        width: `calc(100% - ${25 * 2}px)`,
-    },
-    formLabel: {
-        fontSize: 18,
-    },
-    select: {
-        height: 'auto',
-        borderRadius: 0,
-    },
-    selectIcon: {
-        top: '50%',
-        marginTop: -12,
-    },
-});
-
-function TextField(props: { [x: string]: any; classes: any; InputProps?: {} | undefined; InputLabelProps: any; noBorder?: false | undefined; size?: "medium" | undefined; SelectProps: any; }) {
-    const {
-        classes,
-        InputProps = {},
-        InputLabelProps,
-        noBorder = false,
-        size = 'medium',
-        SelectProps,
-        ...other
-    } = props;
-
-    const {
-        // @ts-ignore
-        classes: { input: InputPropsClassesInput, ...InputPropsClassesOther } = {},
-        ...InputPropsOther
-    } = InputProps;
-
-    return (
-        <MuiTextField
-            InputProps={{
-                disableUnderline: true,
-                classes: {
-                    root: classes.root,
-                    input: clsx(
-                        classes.input,
-                        classes[`inputSize${capitalize(size)}`],
-                        {
-                            [classes.inputBorder]: !noBorder,
-                        },
-                        InputPropsClassesInput,
-                    ),
-                    disabled: classes.disabled,
-                    ...InputPropsClassesOther,
-                },
-                ...InputPropsOther,
-            }}
-            InputLabelProps={{
-                ...InputLabelProps,
-                shrink: true,
-                className: classes.formLabel,
-            }}
-            SelectProps={{
-                ...SelectProps,
-                classes: {
-                    select: classes.select,
-                    icon: classes.selectIcon,
-                },
-            }}
-            {...other}
-        />
-    );
+interface ITextField {
+    label: string;
+    required?: boolean;
+    setState: any;
+    type?: string;
 }
 
-TextField.propTypes = {
-    classes: PropTypes.object.isRequired,
-    InputLabelProps: PropTypes.object,
-    InputProps: PropTypes.object,
-    noBorder: PropTypes.bool,
-    SelectProps: PropTypes.object,
-    size: PropTypes.oneOf(['small', 'medium', 'large', 'xlarge']),
+/***
+ * Login field.
+ */
+export default ({label, required, setState, type}: ITextField) => {
+    return (
+        <TextField
+            variant="filled"
+            margin="normal"
+            required={required}
+            fullWidth
+            id={label}
+            label={label}
+            autoComplete={label}
+            type={type}
+            onChange={(e) => {
+                setState(e.target.value);
+            }}
+        />
+    );
 };
-
-// @ts-ignore
-export default withStyles(styles)(TextField);
