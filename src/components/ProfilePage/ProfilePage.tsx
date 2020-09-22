068a8993ad0ee088139ce1e6ece89b7069435885
Xiancheng Yang <xiancheng@student.unimelb.edu.au>
2020-09-22 22:53:57 +1000

    button fix improvement

--- src/components/ProfilePage/ProfilePage.tsx
+++ src/components/ProfilePage/ProfilePage.tsx
@@ -10,6 +10,10 @@ import Footer from '../Footer/AppFooter';
 import Parallax from './Parallax';
 import Button from '../Button/Button';
 import {ButtonGroup} from '@material-ui/core';
+import Fab from '@material-ui/core/Fab';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import {Grid} from '@material-ui/core';
 
 // @ts-ignore
 const useStyles: any = makeStyles((theme: Theme) =>
@@ -55,7 +59,6 @@ const useStyles: any = makeStyles((theme: Theme) =>
             minHeight: '32px',
         },
         social: {
-            align: 'centre',
             margin: 'auto',
             textAlign: 'center',
             marginBottom: '500px',
@@ -71,46 +74,61 @@ export default function ProfilePage(props: {[x: string]: any}) {
         classes.imgFluid
     );
 
-    return (
-        <div>
-            <AppBar />
-            <Parallax
-                small
-                filter
-                image="https://comp30002.blob.core.windows.net/image/viewB.jpg"
-            />
-            <div className={classes.profile}>
+    // @ts-ignore
+    let div = (
+        <>
+            <div>
+                <AppBar />
+                <Parallax
+                    small
+                    filter
+                    image="https://comp30002.blob.core.windows.net/image/viewB.jpg"
+                />
+                <div className={classes.profile}>
+                    <div>
+                        <img
+                            src="https://comp30002.blob.core.windows.net/image/profile.png"
+                            alt="..."
+                            className={imageClasses}
+                        />
+                    </div>
+                    <div className={classes.name}>
+                        <h1 className={classes.title}>David Smith</h1>
+                        <h2>(Title)</h2>
+                    </div>
+                </div>
+                <div className={classes.description}>
+                    <p>
+                        An artist of considerable range, Chet Faker — the name
+                        taken by Melbourne-raised, Brooklyn-based Nick Murphy —
+                        writes, performs and records all of his own music,
+                        giving it a warm, intimate feel with a solid groove
+                        structure.{' '}
+                    </p>
+                </div>
                 <div>
-                    <img
-                        src="https://comp30002.blob.core.windows.net/image/profile.png"
-                        alt="..."
-                        className={imageClasses}
-                    />
+                    <Grid
+                        container
+                        className={classes.social}
+                        alignContent="center"
+                        spacing={2}
+                    >
+                        <Grid item>
+                            <Fab color="primary" aria-label="share">
+                                <ShareIcon />
+                            </Fab>
+                        </Grid>
+                        <Grid item>
+                            <Fab color="primary" aria-label="like">
+                                <FavoriteIcon />
+                            </Fab>
+                        </Grid>
+                    </Grid>
                 </div>
-                <div className={classes.name}>
-                    <h1 className={classes.title}>David Smith</h1>
-                    <h2>(Title)</h2>
-                </div>
+
+                <Footer />
             </div>
-            <div className={classes.description}>
-                <p>
-                    An artist of considerable range, Chet Faker — the name taken
-                    by Melbourne-raised, Brooklyn-based Nick Murphy — writes,
-                    performs and records all of his own music, giving it a warm,
-                    intimate feel with a solid groove structure.{' '}
-                </p>
-            </div>
-            <div className={classes.social}>
-                <ButtonGroup
-                    color="secondary"
-                    aria-label="outlined primary button group"
-                >
-                    <Button>Like</Button>
-                    <Button>Comment</Button>
-                    <Button>Share</Button>
-                </ButtonGroup>
-            </div>
-            <Footer />
-        </div>
+        </>
     );
+    return div;
 }
