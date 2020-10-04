666f3be748f59b3ec9aaa4acb600b07b6828822d
Yunfei Jing <yunfei.jing@outlook.com>
2020-10-05 00:24:26 +1100

    test: :white_check_mark: add custom testing util for testing (react testing library)
    more test cases to write, use the testing util in src/utils/

--- src/tests/signin.test.jsx
+++ src/tests/signin.test.jsx
@@ -1,27 +1,14 @@
-import React from 'react';
-import {Provider} from 'react-redux';
-import {configure, mount} from 'enzyme';
-import configureMockStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
+import * as React from 'react';
+import {render} from '../utils/test-utils';
+
 import SignInForm from '../components/AuthDialogs/SignInForm';
-import Adapter from 'enzyme-adapter-react-16';
-import {Button, TextField} from '@material-ui/core';
 
-configure({adapter: new Adapter()});
-const mockStore = configureMockStore([thunk]);
-
-describe('Login tests', () => {
-    const store = mockStore({});
-    const wrapper = mount(
-        <Provider store={store}>
-            <SignInForm />
-        </Provider>
-    );
-    it('should have login button and forget password button', () => {
-        expect(wrapper.find(Button).length).toEqual(4);
-    });
-
-    it('should render the form', () => {
-        expect(wrapper.find(TextField).length).toEqual(2);
-    });
+test('render the correct content', () => {
+    const {getByText} = render(<SignInForm />);
+    getByText(/Welcome Back/);
+    getByText('Username');
+    getByText('Password');
+    getByText('Sign in');
+    getByText('Sign up');
+    getByText('Forgot password?');
 });
--- src/tests/signup.test.jsx
+++ src/tests/signup.test.jsx
@@ -1,32 +1,18 @@
-import React from 'react';
-import {Provider} from 'react-redux';
-import {mount, configure} from 'enzyme';
-import configureMockStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
+import * as React from 'react';
+import {render} from '../utils/test-utils';
+
 import SignUpForm from '../components/AuthDialogs/SignUpForm';
-import Adapter from 'enzyme-adapter-react-16';
-import {TextField, Button} from '@material-ui/core';
-import PhoneInput from 'react-phone-input-2';
 
-configure({adapter: new Adapter()});
-const mockStore = configureMockStore([thunk]);
-
-describe('Login tests', () => {
-    const store = mockStore({});
-    const wrapper = mount(
-        <Provider store={store}>
-            <SignUpForm />
-        </Provider>
-    );
-    it('should have signup button', () => {
-        expect(wrapper.find(Button).length).toEqual(1);
-    });
-
-    it('should render the form', () => {
-        expect(wrapper.find(TextField).length).toEqual(7);
-    });
-
-    it('should render the phone input', () => {
-        expect(wrapper.find(PhoneInput).length).toEqual(1);
-    });
+test('render the correct content', () => {
+    const {getByText, getAllByText} = render(<SignUpForm />);
+    getByText('Enter Details');
+    getAllByText(/Title/);
+    getByText('Username');
+    getByText('First name');
+    getByText('Last name');
+    getByText('Email Address');
+    getByText(/Password/);
+    getByText(/^Please re-enter your password$/i);
+    getByText('Phone');
+    getByText('Please complete sign up form.');
 });
