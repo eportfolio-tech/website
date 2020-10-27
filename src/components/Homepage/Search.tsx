63ca02f3c41c9514429f373108b002447df3ac61
Zijian-Zeng <zijian328@gmail.com>
2020-10-27 21:17:29 +1100

    style: :art: random background homepage

--- src/components/Homepage/Search.tsx
+++ src/components/Homepage/Search.tsx
@@ -6,7 +6,20 @@ import ProductHeroLayout from './SearchLayout';
 
 import SearchBar from '../Search/SearchBar';
 
-import backgroundImage from '../../assets/searchBackground.jpeg';
+//import backgroundImage from '../../assets/searchBackground.jpeg';
+
+const backgrounds = [
+    'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+    'https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80',
+    'https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+    'https://images.unsplash.com/photo-1511649475669-e288648b2339?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80',
+    'https://comp30002.blob.core.windows.net/image/career.jpg',
+    'https://images.unsplash.com/photo-1504384308090-c894fdcc538d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+    'https://images.unsplash.com/photo-1432821596592-e2c18b78144f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+];
+
+const backgroundImage =
+    backgrounds[Math.floor(Math.random() * backgrounds.length)];
 
 // const backgroundImage =
 //     'https://images.unsplash.com/photo-1468434453985-b1ca3b555f00?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80';
