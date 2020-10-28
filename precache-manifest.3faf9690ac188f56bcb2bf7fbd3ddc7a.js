d47548bccf37841c3cbb650f316855c56d79693a
Yunfei Jing <yunfei.jing@outlook.com>
2020-10-28 16:12:30 +1100

    Updates

---
+++ precache-manifest.3faf9690ac188f56bcb2bf7fbd3ddc7a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "151d566a60931bc16e81f2730561d976",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "dd1139e0a9df8e4bd615",
+    "url": "/website/static/css/2.4c6fb935.chunk.css"
+  },
+  {
+    "revision": "17bbb92ecc03cb4921da",
+    "url": "/website/static/css/main.b72133cc.chunk.css"
+  },
+  {
+    "revision": "dd1139e0a9df8e4bd615",
+    "url": "/website/static/js/2.95dd7ab1.chunk.js"
+  },
+  {
+    "revision": "ff3cfff3e28a1c8b68d30905cbd3a3f8",
+    "url": "/website/static/js/2.95dd7ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17bbb92ecc03cb4921da",
+    "url": "/website/static/js/main.886dc2a0.chunk.js"
+  },
+  {
+    "revision": "6246a29268a1e9378cdd",
+    "url": "/website/static/js/runtime-main.26a26a2d.js"
+  },
+  {
+    "revision": "9ac30937ca90ed93c1c07d897a51ee6e",
+    "url": "/website/static/media/404.9ac30937.svg"
+  },
+  {
+    "revision": "90c96153650ec249ea82f5d44c498b78",
+    "url": "/website/static/media/logo.90c96153.svg"
+  }
+]);
